d0cc896c279d31afcd46a7eac7fe97a0514a8cdc
Ebbe Flisbäck <x-ebbe.flisback@modelon.com>
2024-06-10 14:14:34 +0200

    Initial commit, should work

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Repository
+echo "Cloning repository..."
+git clone git@github.com:eflisback/.ubuntu-config.git ~/ubuntu-config
+
+echo "Updating package list... (logs available in ~/ubuntu-setup/apt-log.txt)"
+sudo apt update -y &>~/ubuntu-setup/apt-log.txt && sudo apt upgrade -y &>~/ubuntu-setup/apt-log.txt
+
+touch ~/ubuntu-setup/apt-log.txt
+
+packages=(
+    "i3"
+    "i3lock"
+    "zsh"
+    "alacritty"
+    "picom"
+)
+
+echo "Installing packages..."
+for package in "${packages[@]}"; do
+    sudo apt install "$package" -y &>~/ubuntu-setup/apt-log.txt
+    echo "--- $package installed ---"
+done
+
+declare -A configuration_files=(
+    ["i3"]="~/ubuntu-config/i3/config ~/.config/i3/config"
+    ["zsh"]="~/ubuntu-config/zsh/.zshrc ~/.zshrc"
+    ["alacritty"]="~/ubuntu-config/alacritty/.alacritty.toml ~/.config/alacritty/alacritty.toml"
+)
+
+for name in "${!configuration_files[@]}"; do
+    paths=(${configuration_files[$name]})
+    src=${paths[0]}
+    dest=${paths[1]}
+
+    mkdir -p $(dirname "$dest")
+    ln -s "$src" "$dest"
+
+    echo "--- $name successfully linked to $dest ---"
+done
+
+pictures=(
+    "ubuntu.jpg"
+    "lock-screen.jpg"
+)
+
+echo "Linknig picture files..."
+for picture in "$pictures[@]}"; do
+    ln -s "~/ubuntu-config/pictures/$picture" "~/Pictures/picture"
+done
+
+echo "Set-up complete"
---
+++ zsh/.zshrc
@@ -0,0 +1,27 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Created by newuser for 5.8.1
+source ~/powerlevel10k/powerlevel10k.zsh-theme
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+alias update='sudo apt update -y && sudo apt upgrade -y && sudo snap refresh'
+alias sdn='shutdown -h now'
+alias rbt='reboot'
+
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory
+
+eval "$(zoxide init zsh)"
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
